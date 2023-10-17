bc8219fcd49e2c9d5a4d39f1c42f69eae0eda022
Jaffar <jacepac264@gmail.com>
2023-10-17 04:31:13 +0300

    [add] stable

--- 2/solution.c
+++ 2/solution.c
@@ -1,147 +1,159 @@
 #include "parser.h"
-#include <stdlib.h>
 #include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-static void
-execute_command(const struct expr *e, int read_from, int write_to, int (*pipedes)[2])
-{
-	char* argv_arr[e->cmd.arg_count + 2];
-	argv_arr[0] = e->cmd.exe;
-
-	for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-		argv_arr[i + 1] = e->cmd.args[i];
-
-	argv_arr[e->cmd.arg_count + 1] = NULL;
-
-	if (strcmp(e->cmd.exe, "exit") == 0)
-	// TODO: handle exit | *some command*
-		exit(0);
-	
-	else if (strcmp(e->cmd.exe, "cd") == 0)
-		chdir(e->cmd.args[0]);
-
-	else {
-		pid_t p = fork();
-
-		if (p == 0) {
-			if (read_from)
-			{
-				close(pipedes[read_from - 1][1]);
-				dup2(pipedes[read_from - 1][0], 0);
-			}
-
-			if (write_to) {
-				close(pipedes[write_to - 1][0]);
-				dup2(pipedes[write_to - 1][1], 1);
-			}
-
-			execvp(e->cmd.exe, argv_arr);
-		}
-
-		else {
-			wait(NULL);
-		}
-	}
-
-	return;
+static int get_num_pipes(const struct command_line *line) {
+  int num_pipes = 0;
+  struct expr *expr = line->head;
+  while (expr != NULL) {
+    if (expr->type == EXPR_TYPE_PIPE)
+      num_pipes++;
+    expr = expr->next;
+  }
+  return num_pipes;
 }
 
-static void
-execute_command_line(const struct command_line *line)
-{
-	/* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
-
-	assert(line != NULL);
-	printf("================================\n");
-	printf("Command line:\n");
-	printf("Is background: %d\n", (int)line->is_background);
-	printf("Output: ");
-	if (line->out_type == OUTPUT_TYPE_STDOUT) {
-		printf("stdout\n");
-	} else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-		printf("new file - \"%s\"\n", line->out_file);
-	} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-		printf("append file - \"%s\"\n", line->out_file);
-	} else {
-		assert(false);
-	}
-	printf("Expressions:\n");
-	const struct expr *e = line->head;
-
-	int pipedes[2][2], read_from = 0, write_to = 0;
-
-	while (e != NULL) {
-		if (e->type == EXPR_TYPE_COMMAND) {
-			printf("\tCommand: %s", e->cmd.exe);
-			for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-				printf(" %s", e->cmd.args[i]);
-			printf("\n");
-
-			if (e->next != NULL && e->next->type == EXPR_TYPE_PIPE) {
-				if (read_from == 1) {
-					write_to = 2;
-				}
-				
-				else {
-					write_to = 1;
-				}
-				pipe(pipedes[write_to - 1]);
-			}
-
-			// if (e->next != NULL) {
-			// 	if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-
-			// 	}
-
-			// 	if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-
-			// 	}
-			// }
-
-			execute_command(e, read_from, write_to, pipedes);
-
-		} else if (e->type == EXPR_TYPE_PIPE) {
-			read_from = write_to;
-			write_to = 0;
-
-			printf("\tPIPE\n");
-		} else if (e->type == EXPR_TYPE_AND) {
-			printf("\tAND\n");
-		} else if (e->type == EXPR_TYPE_OR) {
-			printf("\tOR\n");
-		} else {
-			assert(false);
-		}
-		e = e->next;
-	}
+static int get_num_commands(const struct command_line *line) {
+  int num_commands = 0;
+  struct expr *expr = line->head;
+  while (expr != NULL) {
+    if (expr->type == EXPR_TYPE_COMMAND)
+      num_commands++;
+    expr = expr->next;
+  }
+  return num_commands;
 }
 
-int
-main(void)
-{
-	const size_t buf_size = 1024;
-	char buf[buf_size];
-	int rc;
-	struct parser *p = parser_new();
-	while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
-		parser_feed(p, buf, rc);
-		struct command_line *line = NULL;
-		while (true) {
-			enum parser_error err = parser_pop_next(p, &line);
-			if (err == PARSER_ERR_NONE && line == NULL)
-				break;
-			if (err != PARSER_ERR_NONE) {
-				printf("Error: %d\n", (int)err);
-				continue;
-			}
-			execute_command_line(line);
-			command_line_delete(line);
-		}
-	}
-	parser_delete(p);
-	return 0;
+static void initialize_pipes(const struct command_line *line, int num_pipes,
+                             int *redirect_fd, int (*pd)[2]) {
+  for (int i = 0; i < num_pipes; i++)
+    pipe(pd[i]);
+
+  if (line->out_type == OUTPUT_TYPE_FILE_NEW)
+    *redirect_fd = open(line->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+  else if (line->out_type == OUTPUT_TYPE_FILE_APPEND)
+    *redirect_fd = open(line->out_file, O_WRONLY | O_APPEND | O_CREAT, 0644);
+}
+
+static void delete_pipes(int num_pipes, int *redirect_fd, int (*pd)[2]) {
+  for (int i = 0; i < num_pipes; i++) {
+    close(pd[i][0]);
+    close(pd[i][1]);
+  }
+
+  if (*redirect_fd != -1)
+    close(*redirect_fd);
+}
+
+static void execute_commands(const struct command_line *line, int num_pipes,
+                             int *redirect_fd, int (*pd)[2]) {
+  struct expr *e = line->head;
+  int index = 0;
+  while (e != NULL) {
+    if (e->type == EXPR_TYPE_COMMAND) {
+      char *argv_arr[e->cmd.arg_count + 2];
+      argv_arr[0] = e->cmd.exe;
+
+      for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
+        argv_arr[i + 1] = e->cmd.args[i];
+
+      argv_arr[e->cmd.arg_count + 1] = NULL;
+
+      if (strcmp(e->cmd.exe, "cd") == 0) {
+        chdir(e->cmd.args[0]);
+        return;
+      }
+
+      pid_t p = fork();
+      if (p == 0) {
+        if (index > 0) {
+          dup2(pd[index - 1][0], STDIN_FILENO);
+        }
+
+        if (index == num_pipes && *redirect_fd != -1) {
+          dup2(*redirect_fd, STDOUT_FILENO);
+        } else if (index < num_pipes) {
+          dup2(pd[index][1], STDOUT_FILENO);
+        }
+
+        for (int i = 0; i < num_pipes; i++) {
+          close(pd[i][0]);
+          close(pd[i][1]);
+        }
+
+        if (strcmp(e->cmd.exe, "exit") == 0) {
+          char *_true[2] = {"true", NULL};
+          execvp("true", _true);
+        }
+
+        execvp(e->cmd.exe, argv_arr);
+        exit(1);
+      }
+
+      index++;
+    }
+
+    e = e->next;
+  }
+
+  delete_pipes(num_pipes, redirect_fd, pd);
+
+  for (int i = 0; i <= num_pipes; i++) {
+    wait(NULL);
+  }
+}
+
+static void execute_command_line(const struct command_line *line,
+                                 int num_pipes) {
+
+  int redirect_fd = -1;
+  int pd[num_pipes][2];
+
+  initialize_pipes(line, num_pipes, &redirect_fd, pd);
+  execute_commands(line, num_pipes, &redirect_fd, pd);
+}
+
+int main(void) {
+  const size_t buf_size = 1024;
+  char buf[buf_size];
+  int rc;
+  struct parser *p = parser_new();
+  while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
+    parser_feed(p, buf, rc);
+    struct command_line *line = NULL;
+    while (true) {
+      enum parser_error err = parser_pop_next(p, &line);
+      if (err == PARSER_ERR_NONE && line == NULL)
+        break;
+      if (err != PARSER_ERR_NONE) {
+        printf("Error: %d\n", (int)err);
+        continue;
+      }
+
+      int num_pipes = get_num_pipes(line);
+      int num_commands = get_num_commands(line);
+
+      if (num_commands == 1 && strcmp(line->head->cmd.exe, "exit") == 0) {
+        int exit_code = 0;
+        if (line->head->cmd.arg_count)
+          exit_code = atoi(line->head->cmd.args[0]);
+
+        command_line_delete(line);
+        parser_delete(p);
+
+        exit(exit_code);
+      }
+
+      execute_command_line(line, num_pipes);
+      command_line_delete(line);
+    }
+  }
+  parser_delete(p);
+  return 0;
 }
